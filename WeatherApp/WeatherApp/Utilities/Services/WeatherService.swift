981abcd8782ec1fb1a226178410a839e0c6ae6ef
Haider Rathore <70682547+Haiderrathore@users.noreply.github.com>
2024-07-08 13:16:07 +0500

    Added Network layer to project

---
+++ WeatherApp/WeatherApp/Utilities/Services/WeatherService.swift
@@ -0,0 +1,113 @@
+//
+//  WeatherService.swift
+//  WeatherApp
+//
+//  Created by Haider Rathore on 05/07/2024.
+//
+
+import Foundation
+import Combine
+
+struct WeatherResponse: Codable {
+    let current: CurrentWeather
+    let hourly: [HourlyWeather]
+    let daily: [DailyWeather]
+}
+
+struct CurrentWeather: Codable {
+    let dt: TimeInterval
+    let sunrise: TimeInterval
+    let sunset: TimeInterval
+    let temp: Double
+    let feels_like: Double
+    let pressure: Int
+    let humidity: Int
+    let dew_point: Double
+    let uvi: Double
+    let clouds: Int
+    let visibility: Int
+    let wind_speed: Double
+    let wind_deg: Int
+    let weather: [Weather]
+}
+
+struct HourlyWeather: Codable {
+    let dt: TimeInterval
+    let temp: Double
+    let feels_like: Double
+    let pressure: Int
+    let humidity: Int
+    let dew_point: Double
+    let uvi: Double
+    let clouds: Int
+    let visibility: Int
+    let wind_speed: Double
+    let wind_deg: Int
+    let weather: [Weather]
+    let pop: Double
+}
+
+struct DailyWeather: Codable {
+    let dt: TimeInterval
+    let sunrise: TimeInterval
+    let sunset: TimeInterval
+    let temp: Temperature
+    let feels_like: FeelsLike
+    let pressure: Int
+    let humidity: Int
+    let dew_point: Double
+    let wind_speed: Double
+    let wind_deg: Int
+    let weather: [Weather]
+    let clouds: Int
+    let pop: Double
+    let rain: Double?
+}
+
+struct Temperature: Codable {
+    let day: Double
+    let min: Double
+    let max: Double
+    let night: Double
+    let eve: Double
+    let morn: Double
+}
+
+struct FeelsLike: Codable {
+    let day: Double
+    let night: Double
+    let eve: Double
+    let morn: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+class WeatherService {
+    private let apiKey = "2d8bc7c061e2c33d3861f713a3cbec18"
+    private let baseURL = "https://api.openweathermap.org/data/3.0/onecall"
+    private var cancellable: AnyCancellable?
+
+    func fetchWeather(lat: Double, lon: Double) -> AnyPublisher<WeatherResponse, Error> {
+        var components = URLComponents(string: baseURL)!
+        components.queryItems = [
+            URLQueryItem(name: "lat", value: "\(lat)"),
+            URLQueryItem(name: "lon", value: "\(lon)"),
+            URLQueryItem(name: "exclude", value: "minutely"),
+            URLQueryItem(name: "units", value: "metric"),
+            URLQueryItem(name: "appid", value: apiKey)
+        ]
+
+        let url = components.url!
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .decode(type: WeatherResponse.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
+
